4977d56e464dc94445f2104e315971f7d85db0de
MaximilianoLopez <31918166@itbeltran.com.ar>
2025-04-10 12:22:14 -0300

    footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CountryLayoutComponent } from "./country/layouts/countryLayout/countryLayout.component";
+import { FooterComponent } from "./shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CountryLayoutComponent, FooterComponent],
   templateUrl: './app.component.html',
 
 })
 export class AppComponent {
 
-  
+
 }
--- src/app/shared/pages/home-page/home-page.component.ts
+++ src/app/shared/pages/home-page/home-page.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent {
 
 
-  
+
 }
