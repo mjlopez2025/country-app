a574ef28149f01fc37b43ca17a877f6a03811c96
mjlopez <maximilianojlopez@hotmail.com>
2025-04-14 12:21:30 -0300

    130

--- src/app/country/pages/country-page/country-page.component.ts
+++ src/app/country/pages/country-page/country-page.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { TopMenuComponent } from '../../components/top-menu/top-menu.component';
+import { ActivatedRoute } from '@angular/router';
+import countryRoutes from '../../country.routes';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-country-page',
   imports: [],
   templateUrl: './country-page.component.html',
 })
-export class CountryPageComponent { }
+export class CountryPageComponent {
+
+  countryCode = inject(ActivatedRoute).snapshot.params["code"];
+  CountryService = inject(CountryService);
+
+  countryResource = rxResource({
+    request: () => ({code: this.countryCode}),
+    loader: ({ request}) => {
+      return this.CountryService.searchCountryByAlphaCode(request.code);
+    }
+  })
+
+
+}
--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -46,4 +46,22 @@ export class CountryService {
       })
     )
   }
+
+  searchCountryByAlphaCode(code: string) {
+
+    const url = `${API_URL}/alpha/${code}`
+
+
+    return this.http.get<RESTCountry[]>(url)
+    .pipe(
+      map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
+      map((countries) => countries.at(0)),
+      catchError(error => {
+        console.log('Error Fetching', error);
+
+        return throwError(() => new Error (`No se encontró un país con ese código: ${code}`))
+      })
+    )
+  }
+
 }
