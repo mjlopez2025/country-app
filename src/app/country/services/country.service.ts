dd8fbbbe7577834073bd7a34e60bf750ae71b38e
mjlopez <maximilianojlopez@hotmail.com>
2025-04-15 16:15:56 -0300

    137

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { RESTCountry } from '../interfaces/rest-countries.interfaces';
-import { map, Observable, catchError, throwError, delay } from 'rxjs';
+import { map, Observable, catchError, throwError, delay, of } from 'rxjs';
 import { Country } from '../interfaces/country.interface';
 import { CountryMapper } from '../mappers/country.mappers';
 
@@ -15,7 +15,6 @@ export class CountryService {
 
   searchByCapital (query: string): Observable<Country[]> {
     query = query.toLowerCase();
-
 
     return this.http.get<RESTCountry[]>(`${API_URL}/capital/${query}`)
     .pipe(
